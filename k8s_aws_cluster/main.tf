d54731f5608915fa673b08c00c1dfea8fe4c1213
MilindaBackup <ayyosalli567@gmail.com>
2025-10-26 12:38:23 +0000

    fix

--- k8s_aws_cluster/main.tf
+++ k8s_aws_cluster/main.tf
@@ -1,20 +1,20 @@
-# # VPC for EKS
-#     module "vpc" {
-#     source  = "terraform-aws-modules/vpc/aws"
-#     version = "~> 5.0"
+# VPC for EKS
+    module "vpc" {
+    source  = "terraform-aws-modules/vpc/aws"
+    version = "~> 5.0"
 
-#     name = "eks-demo-vpc"
-#     cidr = "10.0.0.0/16"
-#     map_public_ip_on_launch = true
+    name = "eks-demo-vpc"
+    cidr = "10.0.0.0/16"
+    map_public_ip_on_launch = true
 
-#     azs            = ["eu-west-2a", "eu-west-2b"]
-#     public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-#     private_subnets = []
+    azs            = ["eu-west-2a", "eu-west-2b"]
+    public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = []
 
-#     enable_nat_gateway = false
-#     enable_dns_hostnames = true
-#     enable_dns_support   = true
-#     }
+    enable_nat_gateway = false
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    }
 
 # EKS Module
 module "eks" {
