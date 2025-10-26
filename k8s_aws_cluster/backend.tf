00b003999505a1082765c54011430097b158f02b
MilindaBackup <ayyosalli567@gmail.com>
2025-10-26 15:31:14 +0000

    ch

--- k8s_aws_cluster/backend.tf
+++ k8s_aws_cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "k8s-cluster-test-ng"  # Replace with your bucket name
+    bucket = "k8s-cluster-test-ng1"  # Replace with your bucket name
     key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
