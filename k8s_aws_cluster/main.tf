1a1ec78103ed923394b35df6b4adf976f2ffa4fd
MilindaBackup <ayyosalli567@gmail.com>
2025-10-26 15:17:08 +0000

    test

--- k8s_aws_cluster/main.tf
+++ k8s_aws_cluster/main.tf
@@ -7,7 +7,7 @@
     cidr = "10.0.0.0/16"
     map_public_ip_on_launch = true
 
-    azs            = ["eu-west-2a", "eu-west-2b"]
+    azs            = ["us-east-1a", "us-east-1b"]
     public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = []
 
